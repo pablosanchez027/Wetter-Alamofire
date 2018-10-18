cf02af2350ac100e26f5a43ba599b6ad034a9c1c
Alumno <ulsa@COMPU-11.local>
2018-10-18 13:47:59 -0700

    Alamofire loaded

--- Wetter/ViewController.swift
+++ Wetter/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblGrados: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlWetter = "http://api.openweathermap.org/data/2.5/weather?q=Berlin&APPID=3326d4de85a204e001116973cc1e557a&units=metric"
+        
+        
+        Alamofire.request(urlWetter).responseJSON { response in
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
